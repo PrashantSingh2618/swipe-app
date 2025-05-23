3e7a15f603400b060ea1c62d3bb96784fd5c681c
Sagar Garg <sagargupta1417@gmail.com>
2025-05-24 03:03:29 +0530

    Added loader in wishlist

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -9,8 +9,10 @@ import UndoHeader from '@/components/undo-header';
 import styles from './styles.module.scss'
 import Icon from '@/components/ui/icon';
 import { getWishlist, removeFromWishlist } from '@/service';
+import LoadingIcon from '@/public/loading.svg';
 
 export default function Wishlist() {
+  const [loading, setLoading] = useState<boolean>(true);
   const [undoHistory, setUndoHistory] = useState<any[]>([]);
   const [activeTab, setActiveTab] = useState<string>('All');
   const [uniqueDivisions, setUniqueDivisions] = useState<string[]>([]);
@@ -34,12 +36,14 @@ export default function Wishlist() {
   }
 
   const fetchWishlist = async () => {
+    setLoading(true);
     const response = await getWishlist();
     const list = response.items;
     const uniqueDivisions: string[] = Array.from(new Set(list.map((item: any) => item.division_name)));
     setUniqueDivisions(['All', ...uniqueDivisions]);
     setList(list || []);
     handleTabClick(activeTab, list);
+    setLoading(false);
   }
 
   return <div className={styles.wishlistContainer}>
@@ -50,52 +54,58 @@ export default function Wishlist() {
       }}
       hideBackButton
     />
-    <div className={styles.wishlistHeader}>
-      <h2 className={styles.wishlistHeaderTitle}>
-        My Favorites
-      </h2>
-    </div>
-    <div className={styles.wishlistTabsContainer}>
-      {uniqueDivisions.map(division => (
-        <button
-          className={classNames(styles.wishlistTab, {
-            [styles.active]: activeTab === division,
-          })}
-          key={division}
-          onClick={() => {
-            handleTabClick(division, list)
-          }}
-        >
-          {division}
-        </button>
-      ))}
-    </div>
-    <div className={styles.wishlistContent}>
-      {filteredWishlistItems.map(item => (
-        <div className={styles.wishlistItemContainer} key={item.id}>
-          <div className={styles.wishlistItemImage}>
-            <Image src={item.images[0]} alt={item.name} width={164} height={205} />
+    {
+      loading ? <div className="flex flex-1 justify-center items-center h-[90vh]">
+        <div className="w-10 h-10 border-2 border-[#ffa273] border-t-transparent rounded-full animate-spin" />
+      </div>
+        : <>
+          <div className={styles.wishlistHeader}>
+            <h2 className={styles.wishlistHeaderTitle}>
+              My Favorites
+            </h2>
           </div>
-          <div className={styles.wishlistItemDetails}>
-            <h3 className="text-xs text-balance">{item.name}</h3>
-            <p className="text-xs text-balance">${item.price}</p>
+          <div className={styles.wishlistTabsContainer}>
+            {uniqueDivisions.map(division => (
+              <button
+                className={classNames(styles.wishlistTab, {
+                  [styles.active]: activeTab === division,
+                })}
+                key={division}
+                onClick={() => {
+                  handleTabClick(division, list)
+                }}
+              >
+                {division}
+              </button>
+            ))}
           </div>
-          <div className={styles.wishlistItemColors}>
-            {
-              item.colors.map((color: string) => (
-                <div className={styles.wishlistItemColorContainer} key={color}>
-                  <div className={styles.wishlistItemColor} style={{ backgroundColor: color }} />
+          <div className={styles.wishlistContent}>
+            {filteredWishlistItems.map(item => (
+              <div className={styles.wishlistItemContainer} key={item.id}>
+                <div className={styles.wishlistItemImage}>
+                  <Image src={item.images[0]} alt={item.name} width={164} height={205} />
                 </div>
-              ))
-            }
-          </div>
-          <div className={styles.wishlistIcon} onClick={() => {
-            handleRemoveFromWishlist(item.id)
-          }}>
-            <Icon iconSvg={heartIcon} className='fill-black' />
-          </div>
-        </div>
-      ))}
-    </div>
+                <div className={styles.wishlistItemDetails}>
+                  <h3 className="text-xs text-balance">{item.name}</h3>
+                  <p className="text-xs text-balance">${item.price}</p>
+                </div>
+                <div className={styles.wishlistItemColors}>
+                  {
+                    item.colors.map((color: string) => (
+                      <div className={styles.wishlistItemColorContainer} key={color}>
+                        <div className={styles.wishlistItemColor} style={{ backgroundColor: color }} />
+                      </div>
+                    ))
+                  }
+                </div>
+                <div className={styles.wishlistIcon} onClick={() => {
+                  handleRemoveFromWishlist(item.id)
+                }}>
+                  <Icon iconSvg={heartIcon} className='fill-black' />
+                </div>
+              </div>
+            ))}
+          </div></>
+    }
   </div>;
 }
